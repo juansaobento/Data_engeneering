a5b579acb90e3ba9a4e1693058f2683b0678528b
Juan Sao Bento <juansaobento@gmail.com>
2024-08-22 18:03:19 -0300

    Correcciones segunda pre-entrega

--- Main/main.py
+++ Main/main.py
@@ -19,7 +19,21 @@ def main():
     data=response['drinks']
     datalist= pd.DataFrame(data)
     df= pd.DataFrame(datalist)[['idDrink','strDrink','strCategory','strAlcoholic','strGlass','strInstructions','strIngredient1', 'strIngredient2','strIngredient3', 'strIngredient4','strIngredient5','strIngredient6','strIngredient7','strIngredient8','strIngredient9','strIngredient10','strIngredient11','strIngredient12','strIngredient13','strIngredient14','strIngredient15','strMeasure1', 'strMeasure2', 'strMeasure3', 'strMeasure4', 'strMeasure5', 'strMeasure6', 'strMeasure7', 'strMeasure8', 'strMeasure9', 'strMeasure10', 'strMeasure11', 'strMeasure12', 'strMeasure13', 'strMeasure14', 'strMeasure15']]
+    ## Correcciones segunda pre-entrega
+    
+    def create_ingredients_json(row):
+        ingredients = {}
+        for i in range(1, 16):  # Assuming up to 15 ingredients
+            ingredient = row.get(f'strIngredient{i}')
+            measure = row.get(f'strMeasure{i}')
+            if ingredient and measure:
+                ingredients[ingredient] = measure
+        return json.dumps(ingredients)  # Convert dictionary to JSON string
 
+    df['Ingredientes'] = df.apply(create_ingredients_json, axis=1)
+    df = df[['idDrink', 'strDrink', 'strCategory', 'strAlcoholic', 'strGlass', 'strInstructions', 'Ingredientes']]
+    df= df.rename(columns= {'strDrink':'Cocktail', 'strCategory':'Categoria', 'strAlcoholic':'Alcoholico', 'strGlass':'Cristaleria', 'strInstructions':'Instrucciones'})
+    df
     ## PRE-ENTREGA 2
 
     ##Definicion de credenciales para conexion
